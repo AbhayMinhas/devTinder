1ac5db814975463c6347a4eb063cf4df18ab5cf4
Abhay Minhas <abhayminhas96@gmail.com>
2025-11-19 12:28:16 +0530

    updated DB password

--- src/config/database.js
+++ src/config/database.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 
 
 const connectDB = async ()=>{
-    await mongoose.connect("mongodb+srv://Abhay:abhay%402003@namastenode.ij3gdlm.mongodb.net/devTinder");//if add the name of the specific database at the end of the url it will connect to that database
+    await mongoose.connect("mongodb+srv://Abhay:h3sDZY11CHNAryXA@namastenode.ij3gdlm.mongodb.net/devTinder");//if add the name of the specific database at the end of the url it will connect to that database
     //if not refering to db it is refering to the cluser
     //devtinder will be new database which will be created inside that cluster
 
